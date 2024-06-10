8460c3f36f3c9b47882c588e582b2837e3db23f2
Venkateshjakkam <120975339+Venkateshjakkam@users.noreply.github.com>
2024-06-10 18:35:10 +0530

    Responsive-Design-Commit

--- registration.js
+++ registration.js
@@ -10,6 +10,8 @@ registrationForm.addEventListener("submit", (e) => {
   let EmailAddress = document.getElementById("gmail");
   let PhoneNumber = document.getElementById("pnum");
   let DateOfBirth = document.getElementById("DOB");
+  let gender = document.querySelector('input[name="gender"]:checked');
+  let hobbies = document.querySelector('input[name="Hobbies"]:checked');
   let Bio = document.getElementById("info");
   let SelectYourCountry = document.getElementById("Ctry");
   let ProfilePicture = document.getElementById("picture");
@@ -23,6 +25,7 @@ registrationForm.addEventListener("submit", (e) => {
     console.log(
       `This form has a FullName of ${FullName.value}, username of ${Username.value} , Email of ${EmailAddress.value} , password of ${Password.value} PhoneNumber of ${PhoneNumber.value} DOB ${DateOfBirth.value} belongs to the ${SelectYourCountry.value} national and has a profile pictur ${ProfilePicture.value}`
     );
+	console.log(gender.value,hobbies.value);
 
 	FullName.value = "";
     Username.value = "";
@@ -34,6 +37,9 @@ registrationForm.addEventListener("submit", (e) => {
 	Bio.value = "";
 	SelectYourCountry.value = "";
 	ProfilePicture.value = "";
+	gender.value = "";
+	hobbies.value = "";
+
 
 
   }
